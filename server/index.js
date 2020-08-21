7d8e6358cdff1b9e9568c0a2f56fa19ef6ab07bb
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-08-20 19:55:03 -0700

    adding simple express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../public`));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Proxy server listening on port ${port}`);
+})
