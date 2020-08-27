3b770445e60ba3bf78a83382baa4987242e87737
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-08-27 15:32:04 -0700

    Renders two modules

--- public/render.js
+++
@@ -1,5 +0,0 @@
-// import { Pictures } from '';
-// import ReactDOM from 'react-dom';
-// import React from 'react';
-
-// ReactDOM.render(React.createElement(Pictures), document.getElementById('pictures-area'));
