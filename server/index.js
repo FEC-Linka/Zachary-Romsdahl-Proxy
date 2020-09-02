66809efd3a08e8973f0c4063f83ac3c1d2f571d1
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-09-02 14:11:00 -0700

    CONSOLIDATION

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.use(express.static(`${__dirname}/../public`));
 
 const port = process.env.PORT || 3001;
 
+app.get('/:id', (req,res) => {
+  console.log(req.params)
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
 })
